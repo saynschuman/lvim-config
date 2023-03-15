52a118a30bdf47a1fa3f7aef7ad7c7d05783d044
vitaliy <vitaliy@heliotrope.digital>
2023-03-16 01:38:49 +0200

    lua format added

--- lua/vitaliy/formatters.lua
+++ lua/vitaliy/formatters.lua
@@ -1,15 +1,13 @@
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
+
 formatters.setup {
-  -- { command = "black", filetypes = { "python" } },
-  -- { command = "isort", filetypes = { "python" } },
   {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
-  },
+    extra_args = {"--print-with", "100"},
+    filetypes = {"typescript", "typescriptreact"}
+  }, {
+    command = "lua-format",
+    filetypes = {"lua"},
+    extra_args = {"--indent-width=2"}
+  }
 }
