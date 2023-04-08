cceb48e5c5bcb365175d891d31df9c0c93a6d523
vitaliy <vitaliy@heliotrope.digital>
2023-04-08 21:03:44 +0300

    colorscheme restored

--- lua/vitaliy/keys.lua
+++ lua/vitaliy/keys.lua
@@ -5,4 +5,5 @@ lvim.keys.normal_mode["<leader>ga"] = ":DiffviewFileHistory<cr>"
 lvim.keys.normal_mode["<leader>gf"] = ":DiffviewFileHistory %<cr>"
 lvim.keys.normal_mode["<leader>gq"] = ":DiffviewClose<cr>"
 lvim.keys.normal_mode["<leader>m"] = ":MarkdownPreview<cr>"
+lvim.keys.normal_mode["<C-[>"] = ":CocList diagnostics<CR>"
 
