c133e8ec249989b21f24b144475bd2b50e930cba
vitaliy <vitaliy@heliotrope.digital>
2023-04-08 21:37:52 +0300

    extra args removed from formatters

--- lua/vitaliy/formatters.lua
+++ lua/vitaliy/formatters.lua
@@ -3,7 +3,6 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   {
     command = "prettier",
-    extra_args = {"--print-with", "100"},
     filetypes = {"typescript", "typescriptreact"}
   }, {
     command = "lua-format",
