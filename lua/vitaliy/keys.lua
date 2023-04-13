10699454c0ccb9f06784ae5430a1a561761ec651
vitaliy <vitaliy@heliotrope.digital>
2023-04-13 15:32:32 +0300

    hop added

--- lua/vitaliy/keys.lua
+++ lua/vitaliy/keys.lua
@@ -1,4 +1,4 @@
-lvim.keys.normal_mode["<C-g>"] = ":Neotree float git_status<cr>"
+-- lvim.keys.normal_mode["<C-g>"] = ":Neotree float git_status<cr>"
 lvim.keys.normal_mode["<C-b>"] = ":G blame<cr>"
 lvim.keys.normal_mode["<C-j>"] = ":lua require('my_plugins.rest').exec()<cr>"
 lvim.keys.normal_mode["<leader>ga"] = ":DiffviewFileHistory<cr>"
@@ -7,4 +7,5 @@ lvim.keys.normal_mode["<leader>gq"] = ":DiffviewClose<cr>"
 lvim.keys.normal_mode["<leader>m"] = ":MarkdownPreview<cr>"
 lvim.keys.normal_mode["<C-[>"] = ":CocList diagnostics<CR>"
 lvim.keys.normal_mode["<C-a>"] = ":CocCommand eslint.executeAutofix<CR>"
+lvim.keys.normal_mode["<C-k>"] = ":HopWord<CR>"
 
--- lua/vitaliy/plugins.lua
+++ lua/vitaliy/plugins.lua
@@ -24,6 +24,13 @@ return {
     "iamcco/markdown-preview.nvim",
     ft = {"markdown"},
     run = "cd app && yarn install"
-  }, {'neoclide/coc.nvim', branch = 'release'}
+  }, {'neoclide/coc.nvim', branch = 'release'}, {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup {keys = 'etovxqpdygfblzhckisuran'}
+    end
+  }
 
 }
