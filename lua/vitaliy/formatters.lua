c12ae2701e0fde9463d83f2256f83f34385c9b62
vitaliyheliotrope <vitaliy@heliotrope.digital>
2024-07-18 17:20:56 +0300

    php & scala prettier added

--- lua/vitaliy/formatters.lua
+++ lua/vitaliy/formatters.lua
@@ -3,10 +3,13 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   {
     command = "prettier",
-    filetypes = {"typescript", "typescriptreact", "sass", "javascript", "vue", "php"}
+    filetypes = {
+      "typescript", "typescriptreact", "sass", "javascript", "vue", "php",
+      "scala"
+    }
   }, {
     command = "lua-format",
-    filetypes = {"lua"},
+    filetypes = {"lua", "scala"},
     extra_args = {"--indent-width=2"}
   }
 }
