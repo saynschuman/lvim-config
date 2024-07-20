6a57c507d4e7d35463137edd287b6d2b2467eb26
vitaliyheliotrope <vitaliy@heliotrope.digital>
2024-07-20 10:25:26 +0300

    hot keys

--- lua/vitaliy/keys.lua
+++ lua/vitaliy/keys.lua
@@ -1,4 +1,4 @@
--- lvim.keys.normal_mode["<C-g>"] = ":Neotree float git_status<cr>"
+-- Горячие клавиши для нормального режима
 lvim.keys.normal_mode["<C-b>"] = ":G blame<cr>"
 lvim.keys.normal_mode["<C-j>"] = ":lua require('my_plugins.rest').exec()<cr>"
 lvim.keys.normal_mode["<leader>ga"] = ":DiffviewFileHistory<cr>"
@@ -8,10 +8,15 @@ lvim.keys.normal_mode["<leader>m"] = ":MarkdownPreview<cr>"
 lvim.keys.normal_mode["<C-[>"] = ":CocList diagnostics<CR>"
 lvim.keys.normal_mode["<C-a>"] = ":CocCommand eslint.executeAutofix<CR>"
 lvim.keys.normal_mode["<C-k>"] = ":HopWord<CR>"
-lvim.keys.normal_mode["<C-s>"] = "copilot#Accept<CR>"
+-- lvim.keys.normal_mode["<C-s>"] = "copilot#Accept<CR>"
+lvim.keys.normal_mode["<C-s>"] = ":CccPick<cr>"
+-- lvim.keys.normal_mode["<leader>r"] = ":CccConvert<cr>"
+lvim.keys.normal_mode["<C-t>"] = ":CccHighlighterToggle<cr>"
 
 -- Настройка ремапа в режиме визуального выбора
 lvim.keys.visual_mode["<leader>p"] = "\"_dP"
 
+-- Горячие клавиши для режима вставки
 vim.api.nvim_set_keymap("i", "<C-f>", "copilot#Accept('<CR>')", { silent = true, expr = true, script = true })
+
 
