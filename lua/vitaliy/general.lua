ae2591d225613e01352ade03cccb6e5a1bc56b79
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-01 16:33:48 +0300

    cursor fixed

--- lua/vitaliy/general.lua
+++ lua/vitaliy/general.lua
@@ -4,6 +4,9 @@ vim.opt.relativenumber = true
 lvim.builtin.treesitter.playground = true
 lvim.builtin.treesitter.autotag.enable = true
 
+-- disable all cursor‐shape changes, terminal will use свой дефолтный (обычно блок)
+-- vim.opt.guicursor = ""
+
 lvim.builtin.which_key.mappings["T"]["h"] = {
   "<cmd>TSHighlightCapturesUnderCursor<cr>", "Highlight"
 }
